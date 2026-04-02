dd737aeb0bfa73f539f1e89eb04ecb20481b715c
HP <HP@LAPTOP-Q73K3RJ9>
2026-04-03 00:03:52 +0700

    Add project files.

---
+++ ProjectHanima.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "___ PROJECT HANIMA ___" << std::endl;
+	std::cout << "Status: Awkaening Hanima..." << std::endl;
+	std::cout << "Progress: 0.000001% - The very first line of code, and the begining of a legend" << std::endl;
+	std::cout << "Goal: Japan Game Studios." << std::endl;
+
+    return 0;
+}
